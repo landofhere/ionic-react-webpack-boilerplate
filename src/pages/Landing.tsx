722a85c03c3046f0c0b8911eac94dbcb959b1e19
Barry Low <bearcanrun@gmail.com>
2019-11-19 22:59:55 -0800

    Setup jest

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,17 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+
+const Landing: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Landing</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        Content
+      </IonContent>
+    </IonPage>
+  );
+};
